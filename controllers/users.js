953f589a81f579d07ad08065f2f10ba19159c9e4
hayleytaylor29 <hft929@gmail.com>
2021-06-05 16:13:57 -0400

    created users.js with create new user route

---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+//route for new user/create new user
+
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user.js');
+
+//create new user
+router.get('/new', (req, res) => {
+    res.render('users/new.ejs');
+});
+
+router.post('/', (req, res) => {
+    User.create(req.body, (error, createdUser) => {
+        res.redirect('/')
+    })
+})
+
+module.exports = router;
