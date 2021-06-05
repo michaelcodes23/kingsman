d568a91ef3c5ccf38a164dcf3eee141635c84874
Michael <cuencamich@hotmail.com>
2021-06-05 18:22:28 -0400

    Adding sessions controller

---
+++ controllers/sessions.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/users.js');
+const session = require('express-session');
+
+//Sessions - new route
+router.get('/new', (req,res)=>{
+    res.render('new.ejs')
+})
+//New
+//Adding test
+router.post('/', (req, res)=>{
+    console.log(req.body);
+    User.findOne({"username": req.body.username}, (error, foundUser)=>{
+        console.log(foundUser)
+
+        if(foundUser[0].password === null){
+            res.redirect('/new')
+        } else if (req.body.password === foundUser[0].password){
+            console.log(req.session)
+            res.redirect('/room')
+        }
+    })
+})
